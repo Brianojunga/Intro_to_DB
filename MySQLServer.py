7a3da2b800b92a11f6e5f9dbb64b6dbc10dc143d
Brianojunga <brianojunga3@gmail.com>
2025-10-19 21:45:51 +0300

    Add script for database creation

---
+++ MySQLServer.py
@@ -0,0 +1,23 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "7066"
+    )
+
+    if connection.is_connected():
+        cursor = connection.cursor()
+        cursor.execute("CREATE DATABASE alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
+
+except Error as err:
+    print(f"Error connecting to MySQL or creating database: {err}")
+
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection closed.")

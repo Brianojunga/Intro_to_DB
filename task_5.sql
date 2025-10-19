d2c50bcb430aa22bc44956c0451d1b11eed5bca3
Brianojunga <brianojunga3@gmail.com>
2025-10-19 23:28:25 +0300

    Add one entry of customers in db

---
+++ task_5.sql
@@ -0,0 +1,17 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "7066"
+        database = "alx_book_store"
+)
+
+cursor = connection.connector()
+
+sql = "INSERT INTO Customer (customer_id, customer_name, email, address) VALUES (%s, %s, %s, %s)"
+
+values = (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.")
+
+cursor.execute(sql, values)
+connection.commit()
